3607c4c1ca07e0e1962638daed692161384380d9
Jake-M13 <marcinkusj13@gmail.com>
2023-04-25 13:59:34 -0400

    updated

--- app.js
+++ app.js
@@ -1,4 +1,81 @@
-console.log("Hello World!\n==========\n");
+"use strict",
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log("Hello Fetch API");
+
+//Program State
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "B41tHrpNPHrmd6KmwA8QV7UgOuDeiPBc";
+
+
+let savedGifs = [];
+// Select Elements
+
+let feedbackElement = document.querySelector("#feedback")
+let searchInput = document.querySelector("#searchWord")
+let searchBtn = document.querySelector("#submitSearch")
+let gifEle = document.querySelector("#imageContainer > img");
+let imageContainer = document.querySelector("#imageContainer");
+let saveBtn = document.querySelector("#saveBtn");
+let savedGifsContainer = document.querySelector("#savedGifs");
+// Event Handlers
+
+searchBtn.addEventListener("click", (event) => {
+    getGif(searchInput.value);         
+});
+
+saveBtn.addEventListener("click", (event) => {
+// save the current gif
+savedGifs.push({ 
+    src: gifEle.src,
+     alt: gifEle.alt,
+      id:gifEle.getAttribute("data-id"),
+});
+
+
+//add new gif to saved container
+
+let newGif = document.createElement("img");
+newGif.src = gifEle.src;
+newGif.alt = gifEle.alt;
+newGif.id = gifEle.getAttribute("data-id");
+savedGifsContainer.prepend(newGif);
+
+});
+
+
+// function getGif(searchTerm) {
+//     fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchInput.value}`)
+//     .then((res) => res.json())
+//     .then((body) => {
+//                 //Show the gif on the dom
+//                 gifEle.src = body.data.images.original.url;
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//                 //SHow the error on dom
+//                 feedbackEle.textContent = err.message;
+//     });             
+// }
+
+async function getGif(searchTerm) {
+    try{
+        let res =  await fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchTerm}`);
+        let body = await res.json();
+        //Show gif on dom
+        gifEle.src = body.data.images.original.url;
+        //update alt
+        gifEle.alt = body.data.title;
+        //update data-id
+        gifEle.setAttribute("data-id", body.data.id);
+        //display image
+        imageContainer.classList.remove("hidden");
+    } catch (err) {
+        console.error(err);
+        // show err message on dom
+        feedbackEle.textContent = err.message;
+        //Hide img container
+        imageContainer.classList.add("hidden");
+    }
+}
+
